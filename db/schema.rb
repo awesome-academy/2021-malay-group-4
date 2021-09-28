e90652b014d43bdfa40165b938026d015fc43a3f
ngyihkai <ngyihkai12@gmail.com>
2021-09-28 15:52:53 +0800

    reset_pass

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_073213) do
+ActiveRecord::Schema.define(version: 2021_09_28_072931) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_09_24_073213) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
